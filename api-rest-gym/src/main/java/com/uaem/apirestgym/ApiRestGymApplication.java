5617b54d7d0e3812b5abfaf770c5e896aec9645a
Gustavo Jaramillo Lara <gustavojaramillolara@gmail.com>
2023-08-22 20:53:07 -0600

    Creacion del proyecto

---
+++ api-rest-gym/src/main/java/com/uaem/apirestgym/ApiRestGymApplication.java
@@ -0,0 +1,13 @@
+package com.uaem.apirestgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestGymApplication.class, args);
+	}
+
+}
